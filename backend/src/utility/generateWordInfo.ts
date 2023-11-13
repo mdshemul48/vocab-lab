5a67e79c3e5a583468e25e2af12109955ce7a315
MD. Shimul <mdshemul480@gmail.com>
2023-11-13 13:09:48 +0600

    setting max token

--- backend/src/utility/generateWordInfo.ts
+++ backend/src/utility/generateWordInfo.ts
@@ -4,7 +4,7 @@ import { openAiConfig } from '../config/openAiConfig';
 const otherConfig = {
   model: 'gpt-3.5-turbo',
   temperature: 0,
-  max_tokens: 1000,
+  max_tokens: 700,
   top_p: 1,
   frequency_penalty: 0,
   presence_penalty: 0,
